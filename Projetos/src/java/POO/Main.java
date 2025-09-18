171058001cfd44d1036175f2fa4ca3b12b22f226
Davi <garotoparga@gmail.com>
2025-09-18 14:23:05 -0300

    ainda com erros

--- Projetos/src/java/POO/Main.java
+++ Projetos/src/java/POO/Main.java
@@ -2,10 +2,7 @@ package java.POO;
 
 public class Main {
     public static void main(String[] args) {
-        ContaBanco p1 = new ContaBanco();
-        p1.setDono("Davi");
-        p1.setNumConta(0000);
-        p1.statusAtual();
+       System.out.println("Teste feito com sucesso.");
     }
 
 }
