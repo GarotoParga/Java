1be06912d97c5aa9fb0c5f57cb5f5b699383bb9b
Davi <garotoparga@gmail.com>
2025-09-24 18:05:06 -0300

    como estava antes

--- Projetos/src/POO/PROGRAMA07/Main.java
+++ Projetos/src/POO/PROGRAMA07/Main.java
@@ -3,7 +3,7 @@ package POO.PROGRAMA07;
 public class Main {
     public static void main(String[] args) {
         Lutador []l = new Lutador[6];
-        // tinha feito merda
+
         l[0] = new Lutador("Pretty boy", "França", 31, 1.75f, 68.9f,11,2,1);
         l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f,14,2,3);
         l[2] = new Lutador("Snapshadow", "EUA", 35, 1.66f, 80.9f,12,2,1);
