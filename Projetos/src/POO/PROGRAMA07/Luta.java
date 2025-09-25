48fc147a38299470b072544dff281759ba80ef28
Davi <garotoparga@gmail.com>
2025-09-24 23:54:43 -0300

    criando class Luta

---
+++ Projetos/src/POO/PROGRAMA07/Luta.java
@@ -0,0 +1,30 @@
+package POO.PROGRAMA07;
+
+public class Luta {
+    private Lutador desafiante, desafiado;
+    private boolean aprovada;
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+}
