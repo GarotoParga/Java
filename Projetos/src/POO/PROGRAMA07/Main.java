fe6eb83ca70997913dea7fd85208873e6c1669fb
Davi <garotoparga@gmail.com>
2025-09-25 00:29:27 -0300

    Mais códigos no vscode

--- Projetos/src/POO/PROGRAMA07/Main.java
+++ Projetos/src/POO/PROGRAMA07/Main.java
@@ -11,6 +11,11 @@ public class Main {
         l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f,5,4,3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f,12,2,4);
 
-            l[0].status();
+            l[0].apresentar();
+            l[1].apresentar();
+            l[2].apresentar();
+            l[3].apresentar();
+            l[4].apresentar();
+            l[5].apresentar();
     }
 }
