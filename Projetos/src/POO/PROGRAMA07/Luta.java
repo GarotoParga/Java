370e9b5de626f57617c0eaf5c42a541dba465105
Garoto Parga <garotoparga@gmail.com>
2025-09-30 16:29:02 -0300

    projeto de luta e lutadores com classes, feito com sucesso

--- Projetos/src/POO/PROGRAMA07/Luta.java
+++ Projetos/src/POO/PROGRAMA07/Luta.java
@@ -1,6 +1,6 @@
 package POO.PROGRAMA07;
 
-import java.util.Objects;
+import java.util.Random;
 
 public class Luta {
     private Lutador desafiante, desafiado;
@@ -9,7 +9,7 @@ public class Luta {
 
 
     public void marcarLuta(Lutador l1, Lutador l2) {
-        if (Objects.equals(l1.getCategoria(), l2.getCategoria()) && l1 != l2) {
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2) {
             this.setAprovada(true);
             this.desafiado = l1;
             this.desafiante = l2;
@@ -22,11 +22,37 @@ public class Luta {
 
     public void lutar() {
         if (this.isAprovada()) {
+            System.out.println("-----DESAFIADO-----");
             this.getDesafiado().apresentar();
+            System.out.println("-----DESAFIANTE-----");
             this.getDesafiante().apresentar();
-            
+
+            Random aleatorio = new Random();
+
+            int vencedor = aleatorio.nextInt(3);
+            System.out.println("-----RESULTADO-----");
+            switch (vencedor) {
+                case 0:
+                    // Empate
+                    System.out.println("EMPATOU LUTA!");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1:
+                    // Vitória do Desafiado
+                    System.out.println("Vitória do " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2:
+                    // Vitória do Desafiante
+                    System.out.println("Vitória do " + this.desafiante.getNome());
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+            }
+            System.out.println("----------");
         } else {
-            System.out.println("Luta não não pode acontecer! ");
+            System.out.println("A luta não pode acontecer! ");
         }
     }
 
