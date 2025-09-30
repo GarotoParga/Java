1f320c50038d596e0d795149cd3a4a30a4c861e8
Garoto Parga <garotoparga@gmail.com>
2025-09-30 15:35:10 -0300

    arquivo luta implementação amais

--- Projetos/src/POO/PROGRAMA07/Luta.java
+++ Projetos/src/POO/PROGRAMA07/Luta.java
@@ -1,8 +1,34 @@
 package POO.PROGRAMA07;
 
+import java.util.Objects;
+
 public class Luta {
     private Lutador desafiante, desafiado;
+    private int rounds;
     private boolean aprovada;
+
+
+    public void marcarLuta(Lutador l1, Lutador l2) {
+        if (Objects.equals(l1.getCategoria(), l2.getCategoria()) && l1 != l2) {
+            this.setAprovada(true);
+            this.desafiado = l1;
+            this.desafiante = l2;
+        } else {
+            this.setAprovada(false);
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+    }
+
+    public void lutar() {
+        if (this.isAprovada()) {
+            this.getDesafiado().apresentar();
+            this.getDesafiante().apresentar();
+            
+        } else {
+            System.out.println("Luta não não pode acontecer! ");
+        }
+    }
 
     public Lutador getDesafiante() {
         return desafiante;
@@ -27,4 +53,5 @@ public class Luta {
     public void setAprovada(boolean aprovada) {
         this.aprovada = aprovada;
     }
+
 }
